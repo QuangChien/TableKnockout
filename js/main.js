0e50d96ac96bbfc6e8ab891afbb1bcae19ed7434
quangchien <quangchien01.it@gmail.com>
2023-01-19 17:28:17 +0700

    19/01/2023

--- js/main.js
+++ js/main.js
@@ -1,54 +1,56 @@
 var initialData = [
-    { name: "Airi Satou", position: "Accountant", office: "Tokyo", age: 33, startDate: "11/28/2008" },
-    { name: "Angelica Ramos", position: "Chief Executive Officer (CEO)", office: "London", age: 47, startDate: "10/9/2009" },
-    { name: "Ashton Cox", position: "Junior Technical Author", office: "San Francisco", age: 66, startDate: "1/12/2009" },
-    { name: "Bradley Greer", position: "Software Engineer", office: "London", age: 41, startDate: "10/13/2012" },
-    { name: "Brielle Williamson", position: "Integration Specialist", office: "New York", age: 61, startDate: "12/2/2012" },
-    { name: "Caesar Vance", position: "Sales Assistant", office: "New York", age: 46, startDate: "12/6/2011" },
-    { name: "Cara Stevens", position: "Sales Assistant", office: "Edinburgh", age: 21, startDate: "5/3/2011" },
-    { name: "Cara Stevens", position: "Sales Assistant", office: "Edinburgh", age: 21, startDate: "5/3/2011" },
-    { name: "Airi Satou", position: "Accountant", office: "Tokyo", age: 33, startDate: "11/28/2008" },
-    { name: "Angelica Ramos", position: "Chief Executive Officer (CEO)", office: "London", age: 47, startDate: "10/9/2009" },
-    { name: "Ashton Cox", position: "Junior Technical Author", office: "San Francisco", age: 66, startDate: "1/12/2009" },
-    { name: "Bradley Greer", position: "Software Engineer", office: "London", age: 41, startDate: "10/13/2012" },
-    { name: "Brielle Williamson", position: "Integration Specialist", office: "New York", age: 61, startDate: "12/2/2012" },
-    { name: "Caesar Vance", position: "Sales Assistant", office: "New York", age: 46, startDate: "12/6/2011" },
-    { name: "Cara Stevens", position: "Sales Assistant", office: "Edinburgh", age: 21, startDate: "5/3/2011" },
-    { name: "Cara Stevens", position: "Sales Assistant", office: "Edinburgh", age: 21, startDate: "5/3/2011" },
-    { name: "Airi Satou", position: "Accountant", office: "Tokyo", age: 33, startDate: "11/28/2008" },
-    { name: "Angelica Ramos", position: "Chief Executive Officer (CEO)", office: "London", age: 47, startDate: "10/9/2009" },
-    { name: "Ashton Cox", position: "Junior Technical Author", office: "San Francisco", age: 66, startDate: "1/12/2009" },
-    { name: "Bradley Greer", position: "Software Engineer", office: "London", age: 41, startDate: "10/13/2012" },
-    { name: "Brielle Williamson", position: "Integration Specialist", office: "New York", age: 61, startDate: "12/2/2012" },
-    { name: "Caesar Vance", position: "Sales Assistant", office: "New York", age: 46, startDate: "12/6/2011" },
-    { name: "Cara Stevens", position: "Sales Assistant", office: "Edinburgh", age: 21, startDate: "5/3/2011" },
-    { name: "Cara Stevens", position: "Sales Assistant", office: "Edinburgh", age: 21, startDate: "5/3/2011" },
+    { id: 1, name: "Airi Satou", position: "Accountant", office: "Tokyo", age: 33, startDate: "11/28/2008", salary: "162.700" },
+    { id: 2, name: "Angelica Ramos", position: "Chief Executive Officer (CEO)", office: "London", age: 47, startDate: "10/9/2009", salary: "127.04" },
+    { id: 3, name: "Ashton Cox", position: "Junior Technical Author", office: "San Francisco", age: 66, startDate: "1/12/2009", salary: "12.332" },
+    { id: 4, name: "Bradley Greer", position: "Software Engineer", office: "London", age: 41, startDate: "10/13/2012", salary: "12.332" },
+    { id: 5, name: "Brielle Williamson", position: "Integration Specialist", office: "New York", age: 61, startDate: "12/2/2012", salary: "12.332" },
+    { id: 6, name: "Caesar Vance", position: "Sales Assistant", office: "New York", age: 46, startDate: "12/6/2011", salary: "12.332" },
+    { id: 7, name: "Cara Stevens", position: "Sales Assistant", office: "Edinburgh", age: 21, startDate: "5/3/2011", salary: "12.332" },
+    { id: 8, name: "Cara Stevens", position: "Sales Assistant", office: "Edinburgh", age: 21, startDate: "5/3/2011", salary: "12.332" },
+    { id: 9, name: "Airi Satou", position: "Accountant", office: "Tokyo", age: 33, startDate: "11/28/2008", salary: 120 },
+    { id: 10, name: "Angelica Ramos", position: "Chief Executive Officer (CEO)", office: "London", age: 47, startDate: "10/9/2009", salary: "12.332" },
+    { id: 11, name: "Ashton Cox", position: "Junior Technical Author", office: "San Francisco", age: 66, startDate: "1/12/2009", salary: "12.332" },
+    { id: 12, name: "Bradley Greer", position: "Software Engineer", office: "London", age: 41, startDate: "10/13/2012", salary: "12.332" },
+    { id: 13, name: "Brielle Williamson", position: "Integration Specialist", office: "New York", age: 61, startDate: "12/2/2012", salary: "12.332" },
+    { id: 14, name: "Caesar Vance", position: "Sales Assistant", office: "New York", age: 46, startDate: "12/6/2011", salary: "12.332" },
+    { id: 15, name: "Cara Stevens", position: "Sales Assistant", office: "Edinburgh", age: 21, startDate: "5/3/2011", salary: "12.332" },
+    { id: 16, name: "Cara Stevens", position: "Sales Assistant", office: "Edinburgh", age: 21, startDate: "5/3/2011", salary: "12.332"},
+    { id: 17, name: "Airi Satou", position: "Accountant", office: "Tokyo", age: 33, startDate: "11/28/2008", salary: "12.332"},
+    { id: 18, name: "Angelica Ramos", position: "Chief Executive Officer (CEO)", office: "London", age: 47, startDate: "10/9/2009", salary: "12.332" },
+    { id: 19, name: "Ashton Cox", position: "Junior Technical Author", office: "San Francisco", age: 66, startDate: "1/12/2009", salary: "12.332" },
+    { id: 20, name: "Bradley Greer", position: "Software Engineer", office: "London", age: 41, startDate: "10/13/2012", salary: "12.332" },
+    { id: 21, name: "Brielle Williamson", position: "Integration Specialist", office: "New York", age: 61, startDate: "12/2/2012", salary: "12.332" },
+    { id: 22, name: "Caesar Vance", position: "Sales Assistant", office: "New York", age: 46, startDate: "12/6/2011", salary: "12.332" },
+    { id: 23, name: "Cara Stevens", position: "Sales Assistant", office: "Edinburgh", age: 21, startDate: "5/3/2011", salary: "12.332" },
+    { id: 24, name: "Cara Stevens", position: "Sales Assistant", office: "Edinburgh", age: 21, startDate: "5/3/2011", salary: "12.332" },
 ];
 
-function Information(name, position, office, age, startDate, salary, isFiltered = true) {
+function Information(id, name, position, office, age, startDate, salary, match = true) {
+    this.id = id;
     this.name = ko.observable(name);
     this.position = ko.observable(position);
     this.office = ko.observable(office);
     this.age = ko.observable(age);
     this.startDate = ko.observable(startDate);
     this.salary = ko.observable(salary);
-    this.isFiltered = ko.observable(isFiltered);
+    this.match = ko.observable(match);
 }
 
 var PagedGridModel = function(items) {
     // this.items = ko.observableArray(items);
     this.items = ko.observableArray(items.map(function (item) {
-        return new Information(item.name, item.position, item.office, item.age, item.startDate, item.salary, item.isFiltered);
+        return new Information(item.id, item.name, item.position, item.office, item.age, item.startDate, item.salary, item.match);
     }));
 
     this.gridViewModel = new ko.simpleGrid.viewModel({
         data: this.items,
         columns: [
-            { headerText: "Name", rowText: "name"},
-            { headerText: "Position", rowText: "position"},
-            { headerText: "Office", rowText: "office"},
-            { headerText: "Age", rowText: "age"},
-            { headerText: "Start date", rowText: "startDate"},
+            { headerText: "Id", rowText: "id", show: false},
+            { headerText: "Name", rowText: "name", show: true},
+            { headerText: "Position", rowText: "position", show: true},
+            { headerText: "Office", rowText: "office", show: true},
+            { headerText: "Age", rowText: "age", show: true},
+            { headerText: "Start date", rowText: "startDate", show: true},
         ],
         pageSize: 5
     });
--- js/simpleGrid.js
+++ js/simpleGrid.js
@@ -5,110 +5,136 @@
         }
         var columns = [];
         for (var propertyName in data[0]) {
-            columns.push({ headerText: propertyName, rowText: propertyName });
+            columns.push({ headerText: propertyName, rowText: propertyName, show: true });
         }
         return columns;
-    }
-
-    var Options = function(totalShow) {
-        this.totalShowOptions = totalShow;
-    };
-
-    ko.bindingHandlers.toggleClick = {
-        init: function(element, valueAccessor){
-            var observable = valueAccessor();
-            ko.utils.registerEventHandler(element, "click", function () {
-                var val = observable();
-                observable(!val);
-            });
         }
-    };
 
-    ko.simpleGrid = {
-        viewModel: function (configuration) {
-            var self = this;
-            self.data = ko.observableArray(configuration.data());
-            this.currentPageIndex = ko.observable(0);
-            this.pageSize = ko.observable(configuration.pageSize);
+        var Options = function(totalShow) {
+            this.totalShowOptions = totalShow;
+        };
 
-            this.sorting = ko.observable(false);
-
-            this.availableOptions = ko.observableArray([
-                new Options(5),
-                new Options(10),
-                new Options(15),
-                new Options(20),
-            ]);
-
-            this.selectedOption = ko.observable();
-            // If you don't specify columns configuration, we'll use scaffolding
-            this.columns = configuration.columns || getColumnsForScaffolding(ko.utils.unwrapObservable(this.data()));
-
-            this.itemsOnCurrentPage = ko.pureComputed(function () {
-                var startIndex = Number(this.pageSize()) * this.currentPageIndex();
-                if(startIndex > this.totalRecord) {
-                    this.currentPageIndex(1);
-                    startIndex = Number(this.pageSize()) * this.currentPageIndex();
-                }
-                var newData = ko.utils.arrayFilter(self.data(), function(item) {
-                    return item.isFiltered(true);
+        ko.bindingHandlers.toggleClick = {
+            init: function(element, valueAccessor){
+                var observable = valueAccessor();
+                ko.utils.registerEventHandler(element, "click", function () {
+                    var val = observable();
+                    observable(!val);
                 });
-                this.sorting = !this.sorting;
-                console.log("change")
-                return newData.slice(startIndex, startIndex + Number(this.pageSize()));
-            }, this).extend({ notify: 'always' });
+            }
+        };
 
-            this.maxPageIndex = ko.computed(function () {
-                return Math.ceil(ko.utils.unwrapObservable(this.data()).length / this.pageSize()) - 1;
-            }, this);
+        ko.simpleGrid = {
+            viewModel: function (configuration) {
+                var self = this;
+                self.data = ko.observableArray(configuration.data());
+                this.currentPageIndex = ko.observable(0);
+                this.pageSize = ko.observable(configuration.pageSize);
 
-            this.previousAction = function() {
-                if(this.currentPageIndex() === 0){
-                    this.currentPageIndex(0);
-                }else{
-                    this.currentPageIndex(this.currentPageIndex() - 1);
+                this.sorting = ko.observable(false);
+
+                this.availableOptions = ko.observableArray([
+                    new Options(5),
+                    new Options(10),
+                    new Options(15),
+                    new Options(20),
+                ]);
+
+                this.selectedOption = ko.observable();
+                // If you don't specify columns configuration, we'll use scaffolding
+                this.columns = configuration.columns || getColumnsForScaffolding(ko.utils.unwrapObservable(this.data()));
+                // console.log(this.columns)
+
+                this.itemsOnCurrentPage = ko.pureComputed(function () {
+                    var startIndex = Number(this.pageSize()) * this.currentPageIndex();
+                    if(startIndex > this.totalRecord) {
+                        this.currentPageIndex(1);
+                        startIndex = Number(this.pageSize()) * this.currentPageIndex();
+                    }
+                    this.filter.value();
+                    var newData = ko.utils.arrayFilter(self.data(), function(item) {
+                        return item.match() === true;
+                    });
+                    console.log("change")
+                    return newData.slice(startIndex, startIndex + Number(this.pageSize()));
+                }, this);
+                // console.log(this.itemsOnCurrentPage())
+                this.maxPageIndex = ko.computed(function () {
+                    var newData = ko.utils.arrayFilter(self.data(), function(item) {
+                        return item.match() === true;
+                    });
+                    return Math.ceil(ko.utils.unwrapObservable(newData).length / this.pageSize()) - 1;
+                }, this);
+
+                this.previousAction = function() {
+                    if(this.currentPageIndex() === 0){
+                        this.currentPageIndex(0);
+                    }else{
+                        this.currentPageIndex(this.currentPageIndex() - 1);
+                    }
                 }
-            }
 
-            this.nextAction = function() {
-                if(this.currentPageIndex() === this.maxPageIndex()){
-                    this.currentPageIndex(this.maxPageIndex());
-                }else{
-                    this.currentPageIndex(this.currentPageIndex() + 1);
+                this.nextAction = function() {
+                    if(this.currentPageIndex() === this.maxPageIndex()){
+                        this.currentPageIndex(this.maxPageIndex());
+                    }else{
+                        this.currentPageIndex(this.currentPageIndex() + 1);
+                    }
                 }
-            }
 
-            this.numberTo = function() {
-                if(this.totalRecord - (Number(this.pageSize()) * this.currentPageIndex()) > Number(this.pageSize())){
-                    return (Number(this.pageSize()) * this.currentPageIndex()) + Number(this.pageSize());
-                }else{
-                    return this.totalRecord;
+                this.numberTo = function() {
+                    var newData = ko.utils.arrayFilter(self.data(), function(item) {
+                        return item.match() === true;
+                    });
+                    if(newData.length - (Number(this.pageSize()) * this.currentPageIndex()) > Number(this.pageSize())){
+                        return (Number(this.pageSize()) * this.currentPageIndex()) + Number(this.pageSize());
+                    }else{
+                        return newData.length;
+                    }
                 }
-            }
+                this.numberOf = function() {
+                    var newData = ko.utils.arrayFilter(self.data(), function(item) {
+                        return item.match() === true;
+                    });
+                    return newData.length;
+                }
 
-            this.optionsChange = function(obj, event){
-                this.pageSize(event.target.value)
-                // console.log(event.target.value)
-            }
+                this.numberFrom = function(){
+                    var newData = ko.utils.arrayFilter(self.data(), function(item) {
+                        return item.match() === true;
+                    });
+                    if(newData.length === 0){
+                        return 0;
+                    }else{
+                        return (Number(self.pageSize()) * self.currentPageIndex()) + 1;
+                    }
+                }
 
-            this.totalRecord = this.data().length;
 
-            this.filter = {
+                this.optionsChange = function(obj, event){
+                    this.pageSize(event.target.value)
+                    // console.log(event.target.value)
+                }
+
+                this.totalRecord = self.data().length;
+
+                this.filter = {
                 value: ko.observable()
             }
 
             this.filter.value.subscribe(function(newValue) {
                 newValue = newValue.toLowerCase();
                 self.data().forEach(function(item) {
-                    if (item.name().toLowerCase().indexOf(newValue) > -1
-                        || item.position().toLowerCase().indexOf(newValue) > -1
-                        || item.office().toLowerCase().indexOf(newValue) > -1
-                        || String(item.age()).indexOf(newValue) > -1
-                        || item.startDate().toLowerCase().indexOf(newValue) > -1
+                    if (item.name().toLowerCase().indexOf(newValue) >= 0
+                        || item.position().toLowerCase().indexOf(newValue) >= 0
+                        || item.office().toLowerCase().indexOf(newValue) >= 0
+                        || String(item.age()).indexOf(newValue) >= 0
+                        || item.startDate().toLowerCase().indexOf(newValue) >= 0
                     ) {
-                        item.isFiltered(true);
+                        item.match(true);
+
                     } else {
-                        item.isFiltered(false);
+                        item.match(false);
                     }
                 });
                 // filterData(newValue);
@@ -116,12 +142,93 @@
 
             this.sortData = function(data, event){
                 this.sorting = !this.sorting;
-                console.log(this.sorting);
+                var parentElement = event.target.parentElement;
+                // Array.from(parentElement.children).forEach(function(item) {
+                //     item.classList.remove("sorting_asc");
+                //     item.classList.remove("sorting_desc");
+                // });
+                if(data.rowText === "name"){
+                    if(event.target.classList.contains("sorting_asc")) {
+                        event.target.classList.remove("sorting_asc");
+                        event.target.classList.add("sorting_desc");
+                        // parentNode.nextElementSibling.style.display = "none";
+                    }else{
+                        event.target.classList.remove("sorting_desc");
+                        event.target.classList.add("sorting_asc");
+                    }
+                }
+                if(data.rowText === "position"){
+                    if(event.target.classList.contains("sorting_asc")) {
+                        event.target.classList.remove("sorting_asc");
+                        event.target.classList.add("sorting_desc");
+                        // parentNode.nextElementSibling.style.display = "none";
+                    }else{
+                        event.target.classList.remove("sorting_desc");
+                        event.target.classList.add("sorting_asc");
+                    }
+                }
+                if(data.rowText === "office"){
+                    if(event.target.classList.contains("sorting_asc")) {
+                        event.target.classList.remove("sorting_asc");
+                        event.target.classList.add("sorting_desc");
+                        // parentNode.nextElementSibling.style.display = "none";
+                    }else{
+                        event.target.classList.remove("sorting_desc");
+                        event.target.classList.add("sorting_asc");
+                    }
+                }
+                if(data.rowText === "age"){
+                    if(event.target.classList.contains("sorting_asc")) {
+                        event.target.classList.remove("sorting_asc");
+                        event.target.classList.add("sorting_desc");
+                        // parentNode.nextElementSibling.style.display = "none";
+                    }else{
+                        event.target.classList.remove("sorting_desc");
+                        event.target.classList.add("sorting_asc");
+                    }
+                }
+                if(data.rowText === "startDate"){
+                    if(event.target.classList.contains("sorting_asc")) {
+                        event.target.classList.remove("sorting_asc");
+                        event.target.classList.add("sorting_desc");
+                        // parentNode.nextElementSibling.style.display = "none";
+                    }else{
+                        event.target.classList.remove("sorting_desc");
+                        event.target.classList.add("sorting_asc");
+                    }
+                }
             }
 
             this.sorting.subscribe(function(newValue){
                 console.log(newValue)
             })
+
+            this.showChild = function(data, event){
+                if(data.rowText === "name"){
+                    var parentNode = event.target.parentElement;
+                    console.log(parentNode.nextElementSibling)
+                    if(parentNode.classList.contains("parent")) {
+                        parentNode.classList.remove("parent");
+                        parentNode.nextElementSibling.style.display = "none";
+                    }else{
+                        parentNode.classList.add("parent");
+                        parentNode.nextElementSibling.style.display = "block";
+                        parentNode.nextElementSibling.style.width = "100%";
+                    }
+                }
+            }
+
+            this.notMatch = function() {
+                var newData = ko.utils.arrayFilter(self.data(), function(item) {
+                    return item.match() === true;
+                });
+                if(newData.length === 0){
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+
             // console.log(this.data())
 
             // function filterData(filter) {
@@ -164,13 +271,26 @@
                     <table style=\"width: 100%;\" class=\"ko-grid display nowrap dataTable dtr-inline collapsed\" cellspacing=\"0\">\
                         <thead>\
                             <tr data-bind=\"foreach: columns\">\
-                               <th class=\"sorting\" data-bind=\"text: headerText, click: $root.sortData, css: { sorting_asc: $root.sortData }\"></th>\
+                               <th class=\"sorting\" data-bind=\"hidden: !show, text: headerText, click: $root.sortData, css: { sorting_asc: rowText === 'name' }\"></th>\
                             </tr>\
                         </thead>\
+                        <tbody data-bind=\"visible: numberOf() === 0\">\
+                        <tr class=\"odd\">\
+                            <td class=\"dataTables_empty\" colspan=\"5\" valign=\"top\">No matching records found</td>\
+                        </tr>\
+                        </tbody>\
                         <tbody data-bind=\"foreach: itemsOnCurrentPage\">\
-                           <tr class=\"odd\" data-bind=\"hidden: !isFiltered(),foreach: $parent.columns\">\
-                               <td data-bind=\"css: {  }, text: typeof rowText == 'function' ? rowText($parent) : $parent[rowText] \"></td>\
-                            </tr>\
+                           <tr data-bind=\"foreach: $parent.columns, css: { old: id % 2 != 0, even: id % 2 === 0, parent: false}\">\
+                               <td data-bind=\"click: $root.showChild, hidden: !show, css: {sorting_1: rowText === 'name', 'dtr-control': rowText === 'name' }, text: typeof rowText == 'function' ? rowText($parent) : $parent[rowText] \"></td>\
+                           </tr>\
+                           <tr class=\"child salary\">\
+                                <td class=\"child\" colspan=\"5\">\
+                                    <ul class=\"dtr-details\" data-dtr-index=\"4\">\
+                                        <li class=\" dt-body-right\" data-dt-column=\"5\" data-dt-row=\"4\" data-dtr-index=\"5\"><span class=\"dtr-title\">Salary</span>\
+                                            $<span data-bind=\"text: salary \" class=\"dtr-data\"></span></li>\
+                                    </ul>\
+                                </td>\
+                           </tr>\
                         </tbody>\
                         <tfoot>\
                             <tr>\
@@ -186,21 +306,22 @@
 
     templateEngine.addTemplate("ko_simpleGrid_info", "\
                     <div class=\"dataTables_info\">\
-                        Showing <span data-bind = \"text: ($root.pageSize() * $root.currentPageIndex()) + 1 \"></span> to \
+                        Showing <span data-bind = \"text: $root.numberFrom() \"></span> to \
                         <span data-bind = \"text: $root.numberTo() \"></span> of \
-                        <span data-bind='text: totalRecord'></span> entries\
+                        <span data-bind='text: numberOf()'></span> entries\
+                        <span data-bind = \"visible: numberOf() < totalRecord \">(filtered from <span data-bind = \"text: totalRecord \"></span> total entries)</span>\
                     </div>");
 
     templateEngine.addTemplate("ko_simpleGrid_pageLinks", "\
                     <div class=\"dataTables_paginate paging_simple_numbers\">\
-                        <a data-bind=\"click: previousAction, css: { disabled: $root.currentPageIndex() == 0 }\" class=\"paginate_button previous\">Previous</a>\
+                        <a data-bind=\"click: previousAction, css: { disabled: $root.currentPageIndex() == 0 || numberOf() === 0 }\" class=\"paginate_button previous\">Previous</a>\
                         <span>\
                         <!-- ko foreach: ko.utils.range(0, maxPageIndex) -->\
                                <a class=\"paginate_button\" href=\"#\" data-bind=\"text: $data + 1, click: function() { $root.currentPageIndex($data) }, css: { current: $data == $root.currentPageIndex() }\">\
                             </a>\
                         <!-- /ko -->\
                         </span>\
-                        <a data-bind=\"click: nextAction, css: { disabled: $root.currentPageIndex() == $root.maxPageIndex() }\" class=\"paginate_button previous\" class=\"paginate_button next\" id=\"example_next\">Next</a>\
+                        <a data-bind=\"click: nextAction, css: { disabled: $root.currentPageIndex() == $root.maxPageIndex() || numberOf() === 0}\" class=\"paginate_button previous\" class=\"paginate_button next\" id=\"example_next\">Next</a>\
                     </div>");
 
     // The "simpleGrid" binding
